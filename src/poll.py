f9fa554f5201a784205aca1f94b8e463cc263fb7
Benjamin Hodgson <benjamin.hodgson@huddle.com>
2015-08-07 14:36:30 +0100

    add doctest

--- src/poll.py
+++ src/poll.py
@@ -14,6 +14,20 @@ def poll(until, timeout=15, interval=1):
         return True if the operation was successful (and retrying should stop)
     :param float timeout: How long to keep retrying the operation in seconds
     :param float interval: How long to sleep in between attempts in seconds
+
+    >>> class TestPoll:
+    ...     def __init__(self):
+    ...         self.x = 0
+    ...     @poll(lambda x: x == 3, interval=0.01)
+    ...     def test(self):
+    ...         print(self.x)
+    ...         self.x += 1
+    ...         return self.x
+    >>> TestPoll().test()
+    0
+    1
+    2
+    3
     """
     def decorator(f):
         @functools.wraps(f)
