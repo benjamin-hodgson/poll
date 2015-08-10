c4be431b346e96084a402d71fcb3a9b2d0f2dbc9
Benjamin Hodgson <benjamin.hodgson@huddle.com>
2015-08-10 17:34:37 +0100

    Added on_error

--- src/poll/__init__.py
+++ src/poll/__init__.py
@@ -2,6 +2,7 @@
 Utilities for polling, retrying, and exception handling.
 """
 import collections
+import inspect
 import time
 from functools import wraps
 
@@ -47,28 +48,37 @@ def poll_(f, until, timeout=15, interval=1, *args, **kwargs):
 
     :return: The final return value of the function ``f``.
     """
-    return exec_(f, (), until, float("inf"), timeout, interval, *args, **kwargs)
+    return exec_(f, (), until, float("inf"), timeout, interval, lambda e, x: None, *args, **kwargs)
 
 
-def retry(ex, times=3, interval=1):
+def retry(ex, times=3, interval=1, on_error=lambda e, x: None):
     """
     Decorator for functions that should be retried upon error.
 
     :param ex: The class of the exception to catch, or an iterable of classes
     :param times: The maximum number of times to retry
     :param interval: The amount of time to sleep between retries
+    :param on_error: A function to be called when the decorated
+        function throws an exception.
+        * If ``on_error()`` takes no parameters, it will be called without arguments.
+        * If ``on_error(exception)`` takes one parameter,
+          it will be called with the exception that was raised.
+        * If ``on_error(exception, retry_count)`` takes two parameters,
+          it will be called with the exception that was raised and the number
+          of previous attempts (starting at 0).
+        A typical use of ``on_error`` would be to log the exception.
 
     :return: The return value of the decorated function
     """
     def decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
-            return retry_(f, ex, times, interval, *args, **kwargs)
+            return retry_(f, ex, times, interval, on_error, *args, **kwargs)
         return wrapper
     return decorator
 
 
-def retry_(f, ex, times=3, interval=1, *args, **kwargs):
+def retry_(f, ex, times=3, interval=1, on_error=lambda e, x: None, *args, **kwargs):
     """
     Call a function and try again if it throws a specified exception.
 
@@ -76,15 +86,23 @@ def retry_(f, ex, times=3, interval=1, *args, **kwargs):
     :param ex: The class of the exception to catch, or an iterable of classes
     :param times: The maximum number of times to retry
     :param interval: The amount of time to sleep between retries
+    :param on_error: A function to be called when ``f`` throws an exception.
+        * If ``on_error()`` takes no parameters, it will be called without arguments.
+        * If ``on_error(exception)`` takes one parameter,
+          it will be called with the exception that was raised.
+        * If ``on_error(exception, retry_count)`` takes two parameters,
+          it will be called with the exception that was raised and the number
+          of previous attempts (starting at 0).
+        A typical use of ``on_error`` would be to log the exception.
 
     Any other arguments are forwarded to ``f``.
 
     :return: The final return value of the function ``f``.
     """
-    return exec_(f, ex, lambda _: True, times, float("inf"), interval, *args, **kwargs)
+    return exec_(f, ex, lambda _: True, times, float("inf"), interval, on_error, *args, **kwargs)
 
 
-def exec_(f, ex, until, times=3, timeout=15, interval=1, *args, **kwargs):
+def exec_(f, ex, until, times=3, timeout=15, interval=1, on_error=lambda e, x: None, *args, **kwargs):
     """
     General function for polling, retrying, and handling errors.
 
@@ -97,6 +115,14 @@ def exec_(f, ex, until, times=3, timeout=15, interval=1, *args, **kwargs):
         (and retrying should stop) and ``False`` if retrying should continue.
     :param times: The maximum number of times to retry
     :param interval: The amount of time to sleep between retries
+    :param on_error: A function to be called when ``f`` throws an exception.
+        * If ``on_error()`` takes no parameters, it will be called without arguments.
+        * If ``on_error(exception)`` takes one parameter,
+          it will be called with the exception that was raised.
+        * If ``on_error(exception, retry_count)`` takes two parameters,
+          it will be called with the exception that was raised and the number
+          of previous attempts (starting at 0).
+        A typical use of ``on_error`` would be to log the exception.
 
     Any other arguments are forwarded to ``f``.
 
@@ -115,6 +141,9 @@ def exec_(f, ex, until, times=3, timeout=15, interval=1, *args, **kwargs):
             if until(result):
                 return result
         except BaseException as e:
+            # if on_error takes 0, 1 or 2 arguments, supply none, one, or both of (e, count)
+            arg_count = len(inspect.signature(on_error).parameters)
+            on_error(*[e, count][:arg_count])
             count += 1
             if count >= times or not isinstance(e, exs):
                 raise
--- test/retry_tests.py
+++ test/retry_tests.py
@@ -31,16 +31,27 @@ class WhenRetryingAFunctionWhichWorksFirstTime:
         return self.x
 
 
-class WhenRetryingAFunctionWhichThrowsAnException:
+class WhenRetryingAFunctionWhichThrowsAnExceptionAndOnErrorTakesNoArguments:
     def given_a_call_counter(self):
         self.x = 0
         self.expected_args = (1, 2, 3)
         self.expected_kwargs = {"foo": "bar"}
         self.args = []
         self.kwargs = []
+        self.on_error_calls = 0
+        self.exception = ValueError()
 
     def when_i_execute_the_retryable_function(self):
-        self.result = self.function_to_retry(*self.expected_args, **self.expected_kwargs)
+        @retry(ValueError, times=3, interval=0.001, on_error=self.on_error)
+        def function_to_retry(*args, **kwargs):
+            self.args.append(args)
+            self.kwargs.append(kwargs)
+            self.x += 1
+            if self.x != 3:
+                raise self.exception
+            return self.x
+
+        self.result = function_to_retry(*self.expected_args, **self.expected_kwargs)
 
     def it_should_keep_trying_until_the_exception_goes_away(self):
         assert self.x == 3
@@ -54,14 +65,93 @@ class WhenRetryingAFunctionWhichThrowsAnException:
     def it_should_return_the_final_answer(self):
         assert self.result is self.x
 
-    @retry(ValueError, times=3, interval=0.001)
-    def function_to_retry(self, *args, **kwargs):
-        self.args.append(args)
-        self.kwargs.append(kwargs)
-        self.x += 1
-        if self.x != 3:
-            raise ValueError
-        return self.x
+    def it_should_call_on_error_every_time_it_failed(self):
+        assert self.on_error_calls == 2
+
+    def on_error(self):
+        self.on_error_calls += 1
+
+
+class WhenRetryingAFunctionWhichThrowsAnExceptionAndOnErrorTakesOneArgument:
+    def given_a_call_counter(self):
+        self.x = 0
+        self.expected_args = (1, 2, 3)
+        self.expected_kwargs = {"foo": "bar"}
+        self.args = []
+        self.kwargs = []
+        self.on_error_calls = []
+        self.exception = ValueError()
+
+    def when_i_execute_the_retryable_function(self):
+        @retry(ValueError, times=3, interval=0.001, on_error=self.on_error)
+        def function_to_retry(*args, **kwargs):
+            self.args.append(args)
+            self.kwargs.append(kwargs)
+            self.x += 1
+            if self.x != 3:
+                raise self.exception
+            return self.x
+
+        self.result = function_to_retry(*self.expected_args, **self.expected_kwargs)
+
+    def it_should_keep_trying_until_the_exception_goes_away(self):
+        assert self.x == 3
+
+    def it_should_forward_the_arguments_every_time(self):
+        assert all(a == self.expected_args for a in self.args)
+
+    def it_should_forward_the_keyword_arguments_every_time(self):
+        assert all(k == self.expected_kwargs for k in self.kwargs)
+
+    def it_should_return_the_final_answer(self):
+        assert self.result is self.x
+
+    def it_should_call_on_error_every_time_it_failed(self):
+        assert self.on_error_calls == [self.exception, self.exception]
+
+    def on_error(self, ex):
+        self.on_error_calls.append(ex)
+
+
+class WhenRetryingAFunctionWhichThrowsAnExceptionOnErrorTakesTwoArguments:
+    def given_a_call_counter(self):
+        self.x = 0
+        self.expected_args = (1, 2, 3)
+        self.expected_kwargs = {"foo": "bar"}
+        self.args = []
+        self.kwargs = []
+        self.on_error_calls = []
+        self.exception = ValueError()
+
+    def when_i_execute_the_retryable_function(self):
+        @retry(ValueError, times=3, interval=0.001, on_error=self.on_error)
+        def function_to_retry(*args, **kwargs):
+            self.args.append(args)
+            self.kwargs.append(kwargs)
+            self.x += 1
+            if self.x != 3:
+                raise self.exception
+            return self.x
+
+        self.result = function_to_retry(*self.expected_args, **self.expected_kwargs)
+
+    def it_should_keep_trying_until_the_exception_goes_away(self):
+        assert self.x == 3
+
+    def it_should_forward_the_arguments_every_time(self):
+        assert all(a == self.expected_args for a in self.args)
+
+    def it_should_forward_the_keyword_arguments_every_time(self):
+        assert all(k == self.expected_kwargs for k in self.kwargs)
+
+    def it_should_return_the_final_answer(self):
+        assert self.result is self.x
+
+    def it_should_call_on_error_every_time_it_failed(self):
+        assert self.on_error_calls == [(self.exception, 0), (self.exception, 1)]
+
+    def on_error(self, ex, retry_count):
+        self.on_error_calls.append((ex, retry_count))
 
 
 class WhenRetryingAFunctionWhichThrowsAnExceptionTooManyTimes:
@@ -166,7 +256,7 @@ class WhenRetryingAFunctionAtTheUseSiteAndItWorksFirstTime:
         self.expected_kwargs = {"foo": "bar"}
 
     def when_i_retry_the_function(self):
-        self.result = retry_(self.function_to_retry, Exception, 3, 0.001, *self.expected_args, **self.expected_kwargs)
+        self.result = retry_(self.function_to_retry, Exception, 3, 0.001, lambda e, x: None, *self.expected_args, **self.expected_kwargs)
 
     def it_should_call_it_once(self):
         assert self.x == 1
@@ -196,7 +286,7 @@ class WhenRetryingAFunctionAtTheUseSiteAndItThrowsAnException:
         self.kwargs = []
 
     def when_i_retry_the_function(self):
-        self.result = retry_(self.function_to_retry, ValueError, 3, 0.001, *self.expected_args, **self.expected_kwargs)
+        self.result = retry_(self.function_to_retry, ValueError, 3, 0.001, lambda e, x: None, *self.expected_args, **self.expected_kwargs)
 
     def it_should_keep_trying_until_the_exception_goes_away(self):
         assert self.x == 3
@@ -266,7 +356,7 @@ class WhenRetryingAFunctionAtTheUseSiteAndListeningForMultipleErrors:
         self.kwargs = []
 
     def when_i_retry_the_function(self):
-        self.result = retry_(self.function_to_retry, [ValueError, IndexError], 3, 0.001, *self.expected_args, **self.expected_kwargs)
+        self.result = retry_(self.function_to_retry, [ValueError, IndexError], 3, 0.001, lambda e, x: None, *self.expected_args, **self.expected_kwargs)
 
     def it_should_keep_trying_until_the_exception_goes_away(self):
         assert self.x == 3
